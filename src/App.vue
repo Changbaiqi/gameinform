9f17599e7b206deaba78a0ca4ca16b0cc239d2b1
Changbaiqi <changbaiqi@foxmail.com>
2025-08-08 20:34:47 +0800

    update: 更新

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ import GameInform from "./components/GameInform.vue";
   margin: 0 auto;
   margin-top: 5vh;
   height: 80vh;
-  background-color: black;
+  background-color: #272727;
   border-radius: 15px;
   box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.3);
   padding: 30px;
@@ -61,4 +61,29 @@ import GameInform from "./components/GameInform.vue";
     opacity: 1;
   }
 }
+/* 容器类名可按需更换 */
+#inform {
+  overflow: auto;
+}
+
+/* WebKit 浏览器（Chrome、Edge、Safari）自定义滚动条 */
+#inform::-webkit-scrollbar {
+  width: 4px;       /* 设置滚动条宽度 */
+  height: 6px;      /* 横向滚动条高度 */
+}
+
+#inform::-webkit-scrollbar-track {
+  background: transparent;     /* 滚动条轨道背景 */
+}
+
+#inform::-webkit-scrollbar-thumb {
+  background-color: rgba(214, 214, 214, 0.3);  /* 滚动条颜色 */
+  border-radius: 10px;                  /* 圆角 */
+}
+
+/* 鼠标悬停滚动条时的样式 */
+#inform::-webkit-scrollbar-thumb:hover {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
 </style>
